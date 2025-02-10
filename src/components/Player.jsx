490f9826c2f218f5d981a26d68ec387abf88cb76
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-11 02:40:08 +0530

    refactor Player component to use state for editable player names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,34 @@
-export default function Player({name, symbol}) {
+import React, { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+   const [playerName, setPlayerName] = useState(initialName); // This is used to store the player name...
+   const [isEditing, setIsEditing] = useState(false);
+
+   const changeHandler = (e) => {
+      setPlayerName(e.target.value); // * This is used to update the player name...
+   };
+
+   const editHandler = () => {
+      // setIsEditing(!isEditing); // * doesn't gurantee the latest state...
+      setIsEditing((prevState) => !prevState); // * Gurantee the latest state, react ensures the latest state...
+   };
+
    return (
       <li>
          <span className="player">
-            <span className="player-name">{name}</span>
+            {!isEditing ? (
+               <span className="player-name">{playerName}</span>
+            ) : (
+               <input
+                  type="text"
+                  value={playerName}
+                  required
+                  onChange={changeHandler}
+               />
+            )}
             <span className="player-symbol">{symbol}</span>
          </span>
-         <button>Edit</button>
+         <button onClick={editHandler}>{isEditing ? "Save" : "Edit"}</button>
       </li>
    );
 }
