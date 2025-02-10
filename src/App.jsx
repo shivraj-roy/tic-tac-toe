1caea06b525a860ec38efe5f445c6f616e6dc58b
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-11 01:25:43 +0530

    add Player component and update App to include player list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
-  return (
-    <p>React Tic-Tac-Toe</p>
-  )
+   return (
+      <main>
+         <div id="game-container">
+            <ol id="players">
+               <Player name="Player 1" symbol="X" />
+               <Player name="Player 2" symbol="O" />
+            </ol>
+            Game Board
+         </div>
+         log
+      </main>
+   );
 }
 
-export default App
+export default App;
