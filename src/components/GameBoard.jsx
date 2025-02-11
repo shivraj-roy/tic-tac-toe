d403c79468faf7ead38c499f6207fa38fd53f1dd
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-11 16:44:17 +0530

    add: 'X' state...

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const initialGameBoard = [
    [null, null, null],
    [null, null, null],
@@ -5,14 +7,32 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+   function selectCellHandler(rowIndex, colIndex) {
+      setGameBoard((prevGameBoard) => {
+         // * Updating state immutably, because we are not allowed to mutate the state directly...
+         const updatedBoard = [
+            ...prevGameBoard.map((innerArray) => [...innerArray]),
+         ]; // * Deep copy of the game board...
+         updatedBoard[rowIndex][colIndex] = "X";
+         return updatedBoard;
+      });
+   }
    return (
       <ol id="game-board">
-         {initialGameBoard.map((row, rowIndex) => (
+         {gameBoard.map((row, rowIndex) => (
             <li key={rowIndex}>
                <ol>
                   {row.map((playerSymbol, cellIndex) => (
                      <li key={cellIndex}>
-                        <button>{playerSymbol}</button>
+                        <button
+                           onClick={() =>
+                              selectCellHandler(rowIndex, cellIndex)
+                           }
+                        >
+                           {playerSymbol}
+                        </button>
                      </li>
                   ))}
                </ol>
