e933bd9a09919739c72ce367ecc845529d83645a
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-13 15:28:42 +0530

    refactor: simplify active player management...

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+   let currentPlayer = "X";
+   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+      currentPlayer = "O";
+   }
+   return currentPlayer;
+}
 function App() {
    const [gameTurns, setGameTurns] = useState([]);
-   const [activePlayer, setActivePlayer] = useState("X");
+   // const [activePlayer, setActivePlayer] = useState("X");
+   const activePlayer = deriveActivePlayer(gameTurns);
 
    function selectCellHandler(rowIndex, colIndex) {
-      setActivePlayer((prevActivePlayer) =>
-         prevActivePlayer === "X" ? "O" : "X"
-      );
+      // setActivePlayer((prevActivePlayer) =>
+      //    prevActivePlayer === "X" ? "O" : "X"
+      // );
       setGameTurns((prevTurns) => {
-         let currentPlayer = 'X';
-         if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-            currentPlayer = 'O';
-         }
+         const currentPlayer = deriveActivePlayer(prevTurns);
          const updatedTurns = [
             { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
             ...prevTurns,
@@ -38,12 +43,9 @@ function App() {
                   isActive={activePlayer === "O"}
                />
             </ol>
-            <GameBoard
-               onSelectCell={selectCellHandler}
-               turns={gameTurns}
-            />
+            <GameBoard onSelectCell={selectCellHandler} turns={gameTurns} />
          </div>
-         <Log turns={gameTurns}/>
+         <Log turns={gameTurns} />
       </main>
    );
 }
